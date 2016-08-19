e7c2ae94f3ba9d5b60874a5b2655af23af372539
Vladislav Baranchuk <kgrpwnz@gmail.com>
2016-08-19 17:27:08 +0300

    Fix spacing

--- src/handlers/common/messages/filmsMessage.js
+++ src/handlers/common/messages/filmsMessage.js
@@ -2,17 +2,15 @@ export default films => [
   '*Сегодня в кино*',
   ...films
     .map(
-      film => {
-        const time = film.time && film.time.join('');
-        return [
-          `*${film.result.info.title}*`,
-          '-',
-          film.seance
-            .map(seance =>
-              seance === time ? `*${seance}*` : seance
-            )
-            .join(', ')
-        ].join(' ');
-      }
+      film => [
+        `*${film.result.info.title}*`,
+        '-',
+        film.seance
+          .map(seance =>
+            seance === (film.time && film.time.join('')) ?
+              `*${seance}*` :
+              seance
+          ).join(', ')
+      ].join(' ')
     )
 ].join("\n\n");
